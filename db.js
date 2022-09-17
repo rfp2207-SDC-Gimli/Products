bff3880e3530f933c2b85711cc492a13cc9c3c2f
darryl-dg <darrylgrierjr@gmail.com>
2022-09-16 17:26:18 -0700

    complete initial code for the following functions: getProducts, getProduct and getRelated

--- db.js
+++ db.js
@@ -9,15 +9,39 @@ const pool = new Pool({
   port: process.env.PGPORT
 });
 
-const testDB = (request, response) => {
-  pool.query('SELECT * FROM public.features LIMIT 100', (err, res) => {
+const getProduct = (request, response) => { //refactor because it only uses product 5
+  pool.query(`SELECT product.id, product.name, product.slogan, product.description, product.category, product.default_price, array_agg(json_build_object('feature', features.feature, 'value', features.value)) AS features
+  FROM product
+  JOIN features ON product.id = features.product_id
+  WHERE product.id = 5
+  GROUP BY product.id, product.name, product.slogan, product.description, product.category, product.default_price`, (err, res) => {
   if (err) {
     throw err
   }
   response.status(200).json(res.rows)
 })
-}
+};
+
+const getProducts = (request, response) => {
+  pool.query(`SELECT * FROM product LIMIT 5;`, (err, res) => {
+  if (err) {
+    throw err
+  }
+  response.status(200).json(res.rows)
+})
+};
+
+const getRelated = (request, response) => {
+  pool.query(`SELECT array_agg(DISTINCT related_product_id) AS results
+  FROM related
+  WHERE current_product_id = 5
+  GROUP BY current_product_id`, (err, res) => {
+  if (err) {
+    throw err
+  }
+  response.status(200).json(res.rows[0])
+})
+};
 
 
-
-module.exports = {testDB};
+module.exports = {getProduct, getProducts, getRelated};
--- index.js
+++ index.js
@@ -5,12 +5,16 @@ const db = require('./db.js')
 const express = require('express')
 const app = express()
 const port = 3001
+app.use(express.json());
+
 
 app.get('/', (req, res) => {
   res.status(200).send('Hello World!');
 })
 
-app.get('/products', db.testDB)
+app.get('/products', db.getProducts)
+app.get('/products/productid', db.getProduct)
+app.get('/products/productid/related', db.getRelated)
 
 app.listen(port, () => {
   console.log(`App running on port ${port}.`)
